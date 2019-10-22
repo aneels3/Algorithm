92cfd70acb7b7eb9a0f0f32271696d5297198385
samit22 <samit@cloudfactory.com>
2019-10-22 17:24:56 +0545

    Add array of numbers sum in ruby.

---
+++ HackerRank Problem/Ruby/add_array_sum.rb
@@ -0,0 +1,13 @@
+# Simple add numbers of array
+# Paste the test function in irb and call test
+
+def sum_array_numbers
+  puts "Enter array of numbers EG: 1.12 123.12 1 134"
+  numbers = gets.chomp
+  num = numbers.split(" ")
+  sum = num.inject(0) { |sum, i| sum + i.to_f }
+  puts "The sum of the numbers is #{sum}"
+end
+
+# or run this file as chmod +x add_sum_array.rb and ./add_sum_array.rb
+sum_array_numbers
