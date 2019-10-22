0133ab09783ebf74a89bd65c7461883bd6f05a8b
Pavan Kuppa <kppa.pavan@gmail.com>
2019-10-22 12:04:41 +0530

    Update Linearsearch.py

--- Linearsearch.py
+++ Linearsearch.py
@@ -1,9 +1,10 @@
 #Linear search algorithm in python 
 def search(array, key): 
-  
+    #iterating through the array
     for i in range(len(array)): 
-  
-        if array[i] == key: 
-            return i 
-  
+      #checking for key value with each element of array
+      if array[i] == key:
+            #if matches return the index
+            return i
+    #else not found
     return -1
