f17bda902d82108ba0995697f646096403fd115f
NANCY ANAND <41587186+nancyanand2807@users.noreply.github.com>
2019-10-22 23:06:50 +0530

    Update rightrotate.py
    Previously the driver code with input was provided. I have edited my code that can now take arguments from the command line. It will first print the inputs given by the user and then right shift the list. Hope this serves the purpose!!

--- rightrotate.py
+++ rightrotate.py
@@ -1,24 +1,25 @@
 # Python program to right rotate a list by n 
-
-
 def rightRotate(lists, num): 
     output_list = [] 
-      
     # Will add values from n to the new list 
     for item in range(len(lists) - num, len(lists)): 
         output_list.append(lists[item]) 
-      
     # Will add the values before 
     # n to the end of new list     
     for item in range(0, len(lists) - num):  
         output_list.append(lists[item]) 
-          
     return output_list 
   
 # Driver Code 
-
-k = 4
-n = [7, 8, 9, 10, 11, 12] 
-  
-print(rightRotate(n, k)) 
+if __name__ == "__main__":
+  list1 = [] 
+# number of elemetns as input 
+  n = int(input("Enter number of elements : ")) 
+# iterating till the range 
+  for i in range(0, n): 
+     ele = int(input()) 
+     list1.append(ele)
+print(list1)
+k = int(input("Enter the number of positions you want to shift:"))
+print(rightRotate(list1, k)) 
 
