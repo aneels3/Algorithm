5b8bc8a1c9056274b1635c9d48fc80d9732d5885
Daniel Castro <daniel.7c.n12@gmail.com>
2019-10-22 13:59:05 +0100

    Removed extra spaces

--- HackerRank Problem/Python/SimpleArraySum.py
+++ HackerRank Problem/Python/SimpleArraySum.py
@@ -14,13 +14,8 @@ def simpleArraySum(ar):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     ar_count = int(input())
-
     ar = list(map(int, input().rstrip().split()))
-
     result = simpleArraySum(ar)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
