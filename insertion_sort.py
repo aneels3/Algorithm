6acb1e41dc6426d5097a245ae32fe78ef82d5074
unknown <waro@god-group.net>
2019-10-22 07:54:58 +0200

    added insertion sort function

---
+++ insertion_sort.py
@@ -0,0 +1,9 @@
+def insertionSort(arr): 
+	# Traverse through 1 to len(arr) 
+	for i in range(1, len(arr)): 
+		key = arr[i] 
+		j = i-1
+		while j >=0 and key < arr[j] : 
+				arr[j+1] = arr[j] 
+				j -= 1
+		arr[j+1] = key
