af3617b9e6a0ec73cb2984a63cb0763bbf7be42a
Pavan Kuppa <kppa.pavan@gmail.com>
2019-10-22 12:32:42 +0530

    Add files via upload

---
+++ Linearsearch.py
@@ -0,0 +1,10 @@
+#Linear search algorithm in python 
+def search(array, key):
+  #iterate in the array
+	for i in range(len(array)):
+	#check each element of array
+		if array[i] == key:
+		#if element matches key,return index
+			return i 
+	#else element not found
+    return -1
