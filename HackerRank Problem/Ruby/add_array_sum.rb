bd828b644fd1aa31312a64d9be00eeea304dd45e
samit22 <samit@cloudfactory.com>
2019-10-23 14:17:55 +0545

    Format hackerRank.

--- HackerRank Problem/Ruby/add_array_sum.rb
+++ HackerRank Problem/Ruby/add_array_sum.rb
@@ -1,13 +1,20 @@
 # Simple add numbers of array
 # Paste the test function in irb and call sum_array_numbers function
 
-def sum_array_numbers
-  puts "Enter array of numbers EG: 1.12 123.12 1 134"
-  numbers = gets.chomp
+def simpleArraySum(numbers)
   num = numbers.split(" ")
-  sum = num.inject(0) { |sum, i| sum + i.to_f }
-  puts "The sum of the numbers is #{sum}"
+  num.inject(0) { |sum, i| sum + i.to_i }
 end
 
-# or run this file as chmod +x add_sum_array.rb and ./add_sum_array.rb
-sum_array_numbers
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+ar_count = gets.to_i
+
+ar = gets.rstrip
+
+result = simpleArraySum(ar)
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
