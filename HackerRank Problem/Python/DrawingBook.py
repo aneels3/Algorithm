746a49f54ed3a9b6573116443f858a44b034f4d4
camfilho <cfmedeiros@ufn.edu.br>
2019-10-23 11:01:08 -0300

    deleted empty lines

--- HackerRank Problem/Python/DrawingBook.py
+++ HackerRank Problem/Python/DrawingBook.py
@@ -1,8 +1,6 @@
 #!/bin/python3
-
 import os
 import sys
-
 #
 # Complete the pageCount function below.
 #
@@ -10,25 +8,13 @@ def pageCount(n, p):
     #
     # Write your code here.
     #
-    
     countFront = p//2
     countBack = n//2 - p//2
-    
     return min(countFront, countBack)
-
-
-    
-
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input())
-
     p = int(input())
-
     result = pageCount(n, p)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
-
