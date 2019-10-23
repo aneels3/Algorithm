8ca9774084ff3eb0a52a151047f59c63a105ce24
Kartik Chopra <kartik.chopra@opstree.com>
2019-10-23 16:46:41 +0530

    Removed unrequired spaces

--- HackerRank Problem/Python/SockMerchant.py
+++ HackerRank Problem/Python/SockMerchant.py
@@ -24,30 +24,19 @@ def bubble_sort(nums):
 def sockMerchant(n, ar):
     count = 0
     ar = bubble_sort(ar)
-    print(ar)
-    # use while loop
     i = 0
     while i < n-1 :
         if ar[i] == ar[i+1]:
-            print(i)
             count = count + 1
             i = i + 2 
         else:
             i = i+1    
-
     return count
-
-
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input())
-
     ar = list(map(int, input().rstrip().split()))
-
     result = sockMerchant(n, ar)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
