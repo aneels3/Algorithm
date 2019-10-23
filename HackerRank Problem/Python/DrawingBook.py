a4756d95c0be584ba966517a404093ca6b77f3eb
camfilho <cfmedeiros@ufn.edu.br>
2019-10-22 23:48:28 -0300

    solve Drawing Book Hacker Rank Challenge

---
+++ HackerRank Problem/Python/DrawingBook.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the pageCount function below.
+#
+def pageCount(n, p):
+    #
+    # Write your code here.
+    #
+    
+    countFront = p//2
+    countBack = n//2 - p//2
+    
+    return min(countFront, countBack)
+
+
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    p = int(input())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
