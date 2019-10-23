140a2a1537a539f7197bbde8db5166b139f0d4b9
Kartik Chopra <kartik.chopra@opstree.com>
2019-10-23 15:57:36 +0530

    Added full code

--- HackerRank Problem/Python/SockMerchant.py
+++ HackerRank Problem/Python/SockMerchant.py
@@ -1,3 +1,12 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
 def bubble_sort(nums):
     # We set swapped to True so the loop looks runs at least once
     swapped = True
@@ -30,3 +39,15 @@ def sockMerchant(n, ar):
 
 
 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
