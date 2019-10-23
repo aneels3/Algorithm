2e4c24997dc280ac641b1cac6ae638fc21435390
deepanshumishra <49090565+deepanshumishra@users.noreply.github.com>
2019-10-23 18:40:03 +0530

    Update A very big sum.py

--- HackerRank Problem/Python/A very big sum.py
+++ HackerRank Problem/Python/A very big sum.py
@@ -15,13 +15,8 @@ def aVeryBigSum(ar):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     ar_count = int(input())
-
     ar = list(map(int, input().rstrip().split()))
-
     result = aVeryBigSum(ar)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
