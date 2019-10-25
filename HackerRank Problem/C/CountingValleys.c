230a606538195c86e71ea9611960a779d27cf4c5
Keith Leonardo <kleonardo52@gmail.com>
2019-10-25 19:46:02 +1100

    Add CountingValleys solution in C

--- HackerRank Problem/C/CountingValleys.c
+++ HackerRank Problem/C/CountingValleys.c
@@ -1,10 +1,17 @@
-// Solution for Counting Valleys problem
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int 
-countingValleys(int n, char* s) {
+char* readline();
+
+// Complete the countingValleys function below.
+int countingValleys(int n, char* s) {
     int count = 0, level = 0;
     int i;
     for(i = 0; i < n; i++) {
@@ -21,18 +28,56 @@ countingValleys(int n, char* s) {
     return count;
 }
 
-int 
-main(int argc, char *argv[]) {
-    if(argc == 2) {
-        int len = strlen(argv[1]);
-        char *str = (char *)malloc((len+1) * sizeof(char));
-        memcpy(str, argv[1], len+1);
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
 
-        int valleys = countingValleys(len, str);
-        
-        printf("Path: %s\n", str);
-        printf("Valley's crossed: %d\n", valleys);
-        free(str);
+    char* n_endptr;
+    char* n_str = readline();
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') {
+        exit(EXIT_FAILURE);
     }
+
+    char* s = readline();
+    int result = countingValleys(n, s);
+    fprintf(fptr, "%d\n", result);
+    fclose(fptr);
+
     return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+        if(!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+        if(data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+        if(!data) {
+            break;
+        }
+
+        alloc_length = new_length;
+    }
+
+    if(data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+    return data;
 }
