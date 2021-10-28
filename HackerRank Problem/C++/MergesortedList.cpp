ce92531a81da4c8e3b99168fef155c202e389384
Srishti <sharmasrishti891@gmail.com>
2021-10-28 17:12:00 +0530

    mergeSortedlist.cpp

---
+++ HackerRank Problem/C++/MergesortedList.cpp
@@ -0,0 +1,27 @@
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL && l2 ==NULL)
+        {
+            return NULL;
+        }
+        if(!l1)
+        {
+            return l2;
+        }
+        if(!l2)
+        {
+            return l1;
+        }
+        if(l1->val >= l2->val)
+        {
+           l2->next= mergeTwoLists(l1,l2->next);
+            return l2;
+        }
+         if(l1->val < l2->val)
+        {
+            
+            l1->next=mergeTwoLists(l1->next,l2);
+             return l1;
+        }
+        
+        
+    return l1;
